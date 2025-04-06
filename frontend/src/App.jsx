2fa663ae3ea7dac3f392be80dc8f4b678fc008c5
Devansh Chauhan <chauhanmansi628@gmail.com>
2025-04-06 11:58:57 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,9 +37,9 @@ function App() {
         </PublicRoutes>
       } />
       <Route path='/dash' element={
-        <PublicRoutes>
+        <ProtectedRoutes>
           <Dashboard/>
-        </PublicRoutes>
+        </ProtectedRoutes>
       } />
     </Routes>
       
